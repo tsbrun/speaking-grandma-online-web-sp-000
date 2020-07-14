e2a4ffc10d9bf67bd73a6648e2a853d571980c2a
TSephanya Brun <tsbrun@icloud.com>
2020-07-14 23:49:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.uppercase
+  if phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
