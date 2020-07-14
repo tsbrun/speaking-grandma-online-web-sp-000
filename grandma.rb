394079f7102b018081f4d4bda511c10330f7897f
TSephanya Brun <tsbrun@icloud.com>
2020-07-14 23:50:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
